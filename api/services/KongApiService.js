30f59206a2af0ba5a0ab42b01e402f93ad2abba8
koolay <koolay@163.com>
2016-06-21 01:10:26 +0800

    kong service

--- api/services/KongApiService.js
+++ api/services/KongApiService.js
@@ -1,7 +1,44 @@
 
-var KongApiService = {
-    request: function() {
+var axios = require('axios');
 
-    }
-}
-module.exports = MyFirstService;
+var errorHandle = function(error) {
+  if (error instanceof Error) {
+    return res.json({result: false, msg: 'server error'});
+  } else {
+    return res.json({result: false, msg: JSON.stringify(error.data), code: error.status});
+  }
+
+};
+var KongApiService = {
+    get: function(api) {
+        axios.get(api)
+        .then(function (response) {
+           return res.view('apis', response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    },
+    post: function(api, param) {
+        axios.post(api, param)
+        .then(function(response){
+           return res.json({result: true, data: JSON.stringify(response.data)});
+        })
+        .catch(function(error){
+            return errorHandle(error);
+        });
+
+    },
+    delete: function(api) {
+
+        axios.delete(api)
+        .then(function(response) {
+            return res.json({result: true});
+        }).catch(function(error){
+            return errorHandle(error);
+        });
+    },
+
+};
+module.exports = KongApiService;
