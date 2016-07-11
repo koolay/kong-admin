4f5269a9f65066d7e5d320c6232c16190490425d
huwl <huwl@mysoft.com.cn>
2016-07-11 11:32:14 +0800

    change bcrypt

--- api/models/User.js
+++ api/models/User.js
@@ -32,7 +32,7 @@ module.exports = {
   beforeCreate : function (values, next) {
     bcrypt.genSalt(10, function (err, salt) {
       if(err) return next(err);
-      bcrypt.hash(values.password, salt, function (err, hash) {
+      bcrypt.hash(values.password, salt, null, function (err, hash) {
         if(err) return next(err);
         values.password = hash;
         next();
